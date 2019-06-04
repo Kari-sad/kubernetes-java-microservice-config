4ffeef2d76dc5c6e75a21392e878e65beee39969
Jamie <jlcoleman@uk.ibm.com>
2019-06-04 13:31:40 +0100

    fix package names for imports

--- start/inventory/src/main/java/inventory/InventoryResource.java
+++ start/inventory/src/main/java/inventory/InventoryResource.java
@@ -20,8 +20,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import io.openliberty.guides.inventory.model.InventoryList;
-import io.openliberty.guides.inventory.client.SystemClient;
+import inventory.model.InventoryList;
+import inventory.client.SystemClient;
 
 @RequestScoped
 @Path("/systems")
